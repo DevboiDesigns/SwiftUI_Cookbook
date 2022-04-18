d73ebe0e2f4ee6e927fbcdc998b7db5932d70e08
Christopher Hicks <visrule@protonmail.com>
2022-04-18 10:17:37 -0500

    Initial commit

---
+++ SwiftUI_Cookbook/SwiftUI_Cookbook/SwiftUI_CookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CookbookApp.swift
+//  SwiftUI_Cookbook
+//
+//  Created by Christopher Hicks on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
